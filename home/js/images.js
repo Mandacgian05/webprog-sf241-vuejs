aefe775e8a60877f90dd704b192fda783096dc73
Gian Mandac <gsmandac@student.apc.edu.ph>
2026-01-13 01:57:25 +0800

    Update images.js

--- home/js/images.js
+++ home/js/images.js
@@ -1,17 +1,18 @@
- const app2 = Vue.createApp({
-   data() {
-    return {
-      manyFoods: [
-        'https://www.w3schools.com/vue/img_burrito.svg',
-        'https://www.w3schools.com/vue/img_salad.svg',
-        'https://www.w3schools.com/vue/img_cake.svg',
-        'https://www.w3schools.com/vue/img_soup.svg',
-        'https://www.w3schools.com/vue/img_fish.svg',
-        'https://www.w3schools.com/vue/img_pizza.svg',
-        'https://www.w3schools.com/vue/img_rice.svg'
-      ]
+
+  const app = Vue.createApp({
+    data() {
+      return {
+        message: "Welcome to my personal website!",
+        manyFoods: [
+          "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=500&q=60",
+          "https://images.unsplash.com/photo-1519681393784-d120267933ba?auto=format&fit=crop&w=500&q=60",
+          "https://images.unsplash.com/photo-1498050108023-c5249f4df085?auto=format&fit=crop&w=500&q=60",
+          "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=500&q=60"
+        ]
+      };
     }
-   }
-  })
-  
-  app2.mount('#app2')
+  });
+
+  app.mount("#app");
+  app.mount("#app2"); // SAME app, not a second app
+
