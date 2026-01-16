4b3b19e12aa63f05d825820660a9a8b7cec0b9d1
Gian Mandac <gsmandac@student.apc.edu.ph>
2026-01-16 15:58:15 +0800

    many change have made

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,32 +1,29 @@
 <template>
   <h1>Coffee!</h1>
-  <p>
-   
-  </p>
-  
-  <FoodItem /> 
-  <FoodItem /> 
-  <FoodItem /> 
-  <FoodItem /> 
+
+  <!-- Food Items -->
+  <FoodItem />
+  <FoodItem />
+  <FoodItem />
+  <FoodItem />
   <FoodItem />
 
-  <FoodItem2 /> 
-  <FoodItem2 /> 
-  <FoodItem2 /> 
-  <FoodItem2 /> 
   <FoodItem2 />
-  
+  <FoodItem2 />
+  <FoodItem2 />
+  <FoodItem2 />
+  <FoodItem2 />
+
+  <!-- Personal Profile Section -->
+  <PersonalProfile />
 </template>
 
 <script setup>
-    import FoodItem from './components/FoodItem.vue' 
+import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
-  import PersonalProfile from "./components/PersonalProfile.vue";
-
-  
+import PersonalProfile from "./components/PersonalProfile.vue"
 </script>
 
-
 <style>
- 
+/* Your styles here */
 </style>
