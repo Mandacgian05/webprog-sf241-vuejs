d0323a9e6f01884fa5a93a3a67534a7544c447e1
Gian Mandac <gsmandac@student.apc.edu.ph>
2026-01-27 15:43:53 +0800

    Update App.vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,13 +1,10 @@
 <template>
   <h1>Food</h1>
-  <country/>
   <food-item/>
-  <food-item/>
-  <food-item/>
+  <food-item2/>
+  <personal-profile/>
   <comment-form/>
   <comment/>
-   
-
 </template>
 
 <script></script>
