0476cc11ac0264261401b42799ec1351140bc2c1
Gian Mandac <gsmandac@student.apc.edu.ph>
2026-02-03 15:27:56 +0800

    Update main.js

--- firstsfc/src/main.js
+++ firstsfc/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
-
 import App from './App.vue'
+
+// Import global components
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
@@ -8,11 +9,20 @@ import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
 import RestApi from './components/RestApi.vue'
 
+// Import Supabase client
+import { supabase } from './supabaseClient'
+
+// Optional: Make Supabase available globally in your app
 const app = createApp(App)
+app.config.globalProperties.$supabase = supabase
+
+// Register global components
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('personal-profile', PersonalProfile)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
 app.component('rest-api', RestApi)
+
+// Mount the app
 app.mount('#app')
